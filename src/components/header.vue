98053671040fabbc4c8f98713432cb148b4a515d
zhanhang <zhome621@gmail.com>
2017-03-24 17:21:16 +0800

    :zap: 优化相关文件的代码结构

--- src/components/header.vue
+++ src/components/header.vue
@@ -3,22 +3,13 @@
 		<header>
 			<ul>
 				<li><router-link to="/movie">电影</router-link></li>
-				<li><router-link to="/douban">高分电影排行榜</router-link></li>
+				<li><router-link to="/movie/hotmovies">正在热映</router-link></li>
+				<li><router-link to="/movie/comingmovies">即将上映</router-link></li>
+				<li><router-link to="/movie/top">高分电影排行榜</router-link></li>
 			</ul>
 		</header>
 	</div>
 </template>
-
-<script>
-	export default {
-		data() {
-			return {
- 				
-			}
-		}
-	}
-</script>
-
 <style scoped lang="scss">
 	.header {
 		display: flex;
